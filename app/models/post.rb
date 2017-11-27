f9f3ccd945fd4846b7b6c349df3fc90faafc4deb
victoria <vbodnar99@gmail.com>
2017-11-27 21:52:44 +0200

    dkjvnd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,6 @@
 class Post < ApplicationRecord
-
-belongs_to :user
-has_many :postcomments 
-
-validates :title, :body, presence: true
-
-validates :title, length:{ in: 1..10}
-
+	belongs_to :user
+	has_many :postcomments
+	validates :title, length: { in: 1..10}
+	validates :title, :body, presence: true
 end
-
--- app/models/postcomment.rb
+++ app/models/postcomment.rb
@@ -1,12 +1,7 @@
 class Postcomment < ApplicationRecord
 	belongs_to :user
 	belongs_to :post
-
-	validates :text, length: { in: 1..400}
-validates :text, presence: true
-validates :text, format: { with: /[a-zA-Z_#][\w.!?,&|@#$%^:'"-]*/ }
-
-
+	validates :text, presence: true
+	validates :text, format: { with:  /[a-zA-Z_#][\w.!?,&|@#$%^:;'"-]*/ }
 end
-
 
